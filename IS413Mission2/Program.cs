ce4be4b192bb00c93087fda182467716a33d890b
eliza1989 <120144963+eliza1989@users.noreply.github.com>
2024-01-17 19:08:37 -0700

    Add files via upload

---
+++ IS413Mission2/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Net.Http.Headers;
+using IS413Mission2;
+
+//Create Roll Results class that will prompt the user and display results
+class RollResults
+{
+    // Create the main method
+    static void Main()
+    {
+        //write prompts to the console welcoming the using and asking them the amount of time they would like to roll
+        Console.WriteLine("Welcome to the dice throwing simulator!");
+        Console.WriteLine("How many dice rolls would you like to simulate?");
+
+        //gather number of rolls from the console and user and create an instance object 
+        diceRolls diceRolls = new diceRolls();
+        int rollNum = int.Parse(Console.ReadLine());
+
+        // create an array for intergers that will store the output of the of the roller method in the diceRolls class
+        int[] diceSums = diceRolls.Roller(rollNum);
+
+        //display explanation of output to the console
+        Console.WriteLine($"\r\nDICE ROLLING SIMULATION RESULTS\r\nEach \"*\" represents 1% of the total number of rolls.\r\nTotal number of rolls = {rollNum}.\n");
+
+        // for the possible roll sums of 2-12 determine the percentage of that result and generate the correct number of *s
+        //then for each result print out the *s to create a histogram
+        for(int i=2; i<=12; i++)
+        {
+            int percent = diceSums[i] * 100 / rollNum;
+            string asteriskCount = new string('*', percent);
+            Console.WriteLine($"{i}: {asteriskCount}");
+        }
+
+        //print end line to the console
+        Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+    }
+}
+
+
+
---
+++ IS413Mission2/diceRolls.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IS413Mission2
+{
+    //Create diceRolls Class
+    internal class diceRolls
+    {
+        // make a private variable for generating random numbers
+        private Random rnd = new Random();
+
+        // create the Roller method that accepts the roll number as a parameter and returns and array of integers
+        public int[] Roller(int rollNum)
+        {
+            // create array with 11 slots for integers
+            int[] diceSums = new int[13];
+
+            //for the amount of rolls desired by the user generate the 2 dice values and their sum, add the sum to the array
+            //and increment the are by 1 each time through the loop
+            for (int i = 0; i < rollNum; i++)
+            {
+                int dice1 = rnd.Next(1, 7);
+                int dice2 = rnd.Next(1, 7);
+                int sum = dice1 + dice2;
+                diceSums[sum]++;
+            }
+
+            //return the array as the result of the method
+            return diceSums;
+        }
+    }
+}
